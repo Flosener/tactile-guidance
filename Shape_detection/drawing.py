81746de4a83f976607aed67421205566c623eda9
BwanaKumbwa <marcin.furtak2@gmail.com>
2024-09-25 13:43:12 +0200

    order 5

--- Shape_detection/drawing.py
+++ Shape_detection/drawing.py
@@ -193,8 +193,8 @@ def alpha():
 
     def draw_categories():
         categories = {
-            'numbers': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            'letters': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'p', 'q', 's', 't', 'u'],
+            'numbers': ['2','5'],
+            'letters': ['s'],
         }
 
         # Shuffle the items within each category for each participant
